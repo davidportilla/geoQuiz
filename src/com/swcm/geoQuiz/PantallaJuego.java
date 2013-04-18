b188fb4a00080d35224e541960a37d18d75da03f
David <david.portilla@outlook.com>
2013-04-18 23:27:05 +0200

    PantallaJuego funciona para una sola pregunta

--- src/com/swcm/geoQuiz/PantallaJuego.java
+++ src/com/swcm/geoQuiz/PantallaJuego.java
@@ -6,38 +6,121 @@ import java.util.List;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.os.Handler;
 import android.util.Log;
-
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.ProgressBar;
+import android.widget.TextView;
 
 public class PantallaJuego extends Activity {
 
+	private TextView mTextView;;
+	private ListView mListView;
+
+	private ProgressBar mProgressBar;
+	private int mProgressStatus;
+	private Handler mHandler = new Handler();
+
+	private boolean itemClicked = false;
+
 	private List<Integer> itemsSelected = new ArrayList<Integer>();
-	
 	private List<String> preguntas = new ArrayList<String>();
 	private List<Integer> soluciones = new ArrayList<Integer>();
 	private List<List<String>> respuestas = new ArrayList<List<String>>();
-	
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		crearJuego();
+
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.pantalla_juego);
+
+		mTextView = (TextView) findViewById(R.id.textView);
+		mListView = (ListView) findViewById(R.id.listView);
+		mProgressBar = (ProgressBar) findViewById(R.id.progressBar);
+
+		mListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+			@Override
+			public void onItemClick(AdapterView<?> arg0, View arg1,
+					int position, long arg3) {
+				itemClicked = true;
+				itemsSelected.add(position);
+				Log.i("ITEMSELECTED", "" + position);
+				if (preguntas.size() != 0) {
+					
+				} else {
+					mTextView.setText("FINAL");
+				}
+
+			}
+		});
+		actualizarPregunta();
+		resetProgressBar();
+	}
+
+	private int doWork() {
+		try {
+			Thread.sleep(100);
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return 1;
+	}
+
+	private void resetProgressBar() {
+		mProgressStatus = 0;
+		// Start lengthy operation in a background thread
+		new Thread(new Runnable() {
+			public void run() {
+				while (mProgressStatus < 100) {
+					mProgressStatus += doWork();
+					// Update the progress bar
+					mHandler.post(new Runnable() {
+						public void run() {
+							mProgressBar.setProgress(mProgressStatus);
+						}
+					});
+				}
+				itemsSelected.add(-1);
+				Log.i("ITEMSELECTED", "-1");
+			}
+		}).start();
+	}
+
+	private void actualizarPregunta() {
+		mTextView.setText(preguntas.get(0));
+		preguntas.remove(0);
+		mListView.setAdapter(new ArrayAdapter<String>(this,
+				android.R.layout.simple_list_item_1, respuestas.get(0)));
+		respuestas.remove(0);
+	}
+
 	// Este método es para probar. Su funcionalidad será parte del modelo
 	private void crearJuego() {
 		soluciones = Arrays.asList(0, 3, 2);
-		preguntas = Arrays.asList("p 1", "p 2", "p 3");
+		preguntas.add("p 1");
+		preguntas.add("p 2");
+		preguntas.add("p 3");
 		final List<String> resp0 = new ArrayList<String>();
 		final List<String> resp1 = new ArrayList<String>();
 		final List<String> resp2 = new ArrayList<String>();
-		resp0.add("resp0"); resp0.add("resp1"); resp0.add("resp2");
-		resp1.add("resp4"); resp1.add("resp5"); resp1.add("resp6"); resp1.add("resp7");
-		resp2.add("resp8"); resp2.add("resp9"); resp2.add("resp10");
+		resp0.add("resp0");
+		resp0.add("resp1");
+		resp0.add("resp2");
+		resp1.add("resp4");
+		resp1.add("resp5");
+		resp1.add("resp6");
+		resp1.add("resp7");
+		resp2.add("resp8");
+		resp2.add("resp9");
+		resp2.add("resp10");
 		respuestas.add(resp0);
 		respuestas.add(resp1);
 		respuestas.add(resp2);
 	}
-	
-	@Override
-	protected void onCreate(Bundle savedInstanceState) {
-		crearJuego();
-		
-		super.onCreate(savedInstanceState);
-		setContentView(R.layout.pantalla_principal);
-	}
-		
+
 }
--- src/com/swcm/geoQuiz/PantallaPrincipal.java
+++ src/com/swcm/geoQuiz/PantallaPrincipal.java
@@ -9,17 +9,11 @@ import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.Gravity;
 import android.view.View;
 import android.widget.Toast;
 
 public class PantallaPrincipal extends Activity {
-
-	/**
-	 * TAG para las trazas
-	 */
-	private static final String TAG = "PantallaPrincipal";
 
 	private static List<Integer> index = new ArrayList<Integer>();
 	
@@ -28,35 +22,7 @@ public class PantallaPrincipal extends Activity {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.pantalla_principal);
 	}
-	
-	private AlertDialog pregunta(String pregunta, String[] respuestas) {
-		AlertDialog.Builder builder = new AlertDialog.Builder(this);
-		builder.setTitle(pregunta).setCancelable(false)
-				.setItems(respuestas, new DialogInterface.OnClickListener() {
-					public void onClick(DialogInterface dialog, int which) {
-						// The 'which' argument contains the index position
-						// of the selected item
-						Log.i(TAG, "valor de which: " + which);
-						switch (which) {
-						case 0:
-							index.add(0);
-							break;
-						case 1:
-							index.add(1);
-							break;
-						case 2:
-							index.add(2);
-							break;
-						case 3:
-							index.add(3);
-						}
-						mostrarResultado();
-					}
-				}).create();
 		
-		return builder.create();
-	}
-	
 	private AlertDialog confirmDialog(String titulo) {
 		AlertDialog.Builder builder = new AlertDialog.Builder(this);
 		builder.setTitle(titulo);
@@ -70,19 +36,6 @@ public class PantallaPrincipal extends Activity {
             }
         });
 		return builder.create();
-	}
-
-	private void mostrarResultado() {
-		Context context = getApplicationContext();
-		CharSequence text = "Respuesta elegida: null";
-		try {
-			text = "Respuesta elegida: " + PantallaPrincipal.index.get(0);
-		} catch(IndexOutOfBoundsException e) {}
-		int duration = Toast.LENGTH_LONG;
-
-		Toast toast = Toast.makeText(context, text, duration);
-		toast.setGravity(Gravity.TOP | Gravity.CENTER, 0, 0);
-		toast.show();
 	}
 	
 	private void about() {
@@ -109,10 +62,6 @@ public class PantallaPrincipal extends Activity {
 		case R.id.capitalesButton:
 			break;
 		}
-	}
-
-	@Override
-	public void onBackPressed() {
 	}
 
 }
